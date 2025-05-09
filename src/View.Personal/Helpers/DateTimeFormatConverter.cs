64d403b8009df39c8631e12dbb7206166f6ed519
Farhan Lodi <farhansl@weblineapps.com>
2025-05-09 12:12:01 +0530

    Worked on below bug - Make sure timezones that are displayed to the user are converted to local timezone.

--- src/View.Personal/Helpers/DateTimeFormatConverter.cs
+++ src/View.Personal/Helpers/DateTimeFormatConverter.cs
@@ -5,35 +5,52 @@ namespace View.Personal.Helpers
     using System.Globalization;
 
     /// <summary>
-    /// Converts DateTime objects or string date representations to a formatted string with the pattern "M/d/yyyy, h:mm:ss tt".
-    /// Handles both DateTime objects and string dates in UTC format.
+    /// Converts DateTime objects or string date representations to a formatted string.
+    /// Handles both DateTime objects and string dates in UTC format, converting them to local time zone.
+    /// Respects the user's system time format preference (12-hour or 24-hour).
     /// </summary>
     public class DateTimeFormatConverter : IValueConverter
     {
         /// <summary>
-        /// Converts a DateTime object or string date representation to a formatted string.
+        /// Converts a DateTime object or string date representation to a formatted string in the local time zone.
+        /// Respects the user's system time format preference (12-hour or 24-hour).
         /// </summary>
         /// <param name="value">The value to convert, either a DateTime object or a string representation of a date.</param>
         /// <param name="targetType">The type of the binding target property (not used).</param>
         /// <param name="parameter">Optional parameter (not used).</param>
-        /// <param name="culture">The culture to use for conversion (not used, always uses InvariantCulture).</param>
+        /// <param name="culture">The culture to use for conversion (not used, uses current culture for format detection).</param>
         /// <returns>
-        /// A string in the format "M/d/yyyy, h:mm:ss tt" (e.g., "3/25/2025, 7:47:03 PM") if conversion is successful;
+        /// A string in the format "M/d/yyyy, h:mm:ss tt" (12-hour) or "M/d/yyyy, HH:mm:ss" (24-hour) in local time zone if conversion is successful;
         /// otherwise, the string representation of the value or an empty string.
         /// </returns>
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
+            // Detect if the system uses 24-hour time format
+            bool uses24HourFormat = !CultureInfo.CurrentCulture.DateTimeFormat.LongTimePattern.Contains("tt");
+            string timeFormat = uses24HourFormat ? "HH:mm:ss" : "h:mm:ss tt";
+            string dateTimeFormat = $"M/d/yyyy, {timeFormat}";
+            
             if (value is string dateString)
             {
                 dateString = dateString.Replace("UTC", "").Trim();
 
                 if (DateTime.TryParse(dateString, CultureInfo.InvariantCulture,
                         DateTimeStyles.AssumeUniversal, out var parsedDate))
-                    return parsedDate.ToString("M/d/yyyy, h:mm:ss tt", CultureInfo.InvariantCulture);
+                {
+                    // Convert UTC time to local time zone
+                    DateTime localDateTime = parsedDate.ToLocalTime();
+                    return localDateTime.ToString(dateTimeFormat, CultureInfo.CurrentCulture);
+                }
             }
 
             if (value is DateTime dateTime)
-                return dateTime.ToString("M/d/yyyy, h:mm:ss tt", CultureInfo.InvariantCulture);
+            {
+                // Assume DateTime values are in UTC and convert to local time
+                DateTime localDateTime = dateTime.Kind == DateTimeKind.Utc ? 
+                                         dateTime.ToLocalTime() : 
+                                         dateTime;
+                return localDateTime.ToString(dateTimeFormat, CultureInfo.CurrentCulture);
+            }
 
             return value?.ToString() ?? string.Empty;
         }
